7ff9ff49afb1f59ca20763023f7e060c60c84b46
Graham Beer <graham.beer@outlook.com>
2023-11-24 11:43:49 +0000

    Update main.go

--- main.go
+++ main.go
@@ -3,10 +3,33 @@ package main
 import (
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/ec2"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
+
+		config := config.New(ctx, "")
+
+		// Stack values
+		IngressIp := config.Require("IngressIp")
+		VpcId := config.Require("VpcId")
+		SubnetId := config.Require("SubnetId")
+
+		// Getting latest Amazon Linux 2 image
+		ami, err := ec2.LookupAmi(ctx, &ec2.LookupAmiArgs{
+			Filters: []ec2.GetAmiFilter{
+				{
+					Name:   "name",                                     // Use a simple string literal for the filter name
+					Values: []string{"amzn2-ami-hvm-2.0.*-x86_64-gp2"}, // Pass a string slice directly for the filter values
+				},
+			},
+			MostRecent: pulumi.BoolRef(true), // Pass a `bool` pointer directly without using pulumi.BoolPtr
+			Owners:     []string{"amazon"},   // Again, pass a string slice directly without wrapping
+		})
+		if err != nil {
+			return err
+		}
 
 		// Security group which allows SSH traffic
 		sg, err := ec2.NewSecurityGroup(ctx, "ec2SecurityGroup", &ec2.SecurityGroupArgs{
@@ -23,40 +46,24 @@ func main() {
 			Ingress: ec2.SecurityGroupIngressArray{
 				ec2.SecurityGroupIngressArgs{
 					CidrBlocks: pulumi.StringArray{
-						pulumi.String("81.103.164.61/32"),
+						pulumi.String(IngressIp),
 					},
 					Protocol: pulumi.String("tcp"),
 					FromPort: pulumi.Int(22), // SSH port
 					ToPort:   pulumi.Int(22),
 				},
 			},
-			VpcId: pulumi.String("vpc-0119db5898b4307f9"),
+			VpcId: pulumi.String(VpcId),
 		})
 		if err != nil {
 			return err
 		}
-
-		// Getting latest Amazon Linux 2 image
-		ami, err := ec2.LookupAmi(ctx, &ec2.LookupAmiArgs{
-			Filters: []ec2.GetAmiFilter{
-				{
-					Name:   "name", // Use a simple string literal for the filter name
-					Values: []string{"amzn2-ami-hvm-2.0.*-x86_64-gp2"}, // Pass a string slice directly for the filter values
-				},
-			},
-			MostRecent: pulumi.BoolRef(true), // Pass a `bool` pointer directly without using pulumi.BoolPtr
-			Owners:     []string{"amazon"}, // Again, pass a string slice directly without wrapping
-		})
-		if err != nil {
-			return err
-		}
-
 
 		// Create EC2 instance with Security group and image
 		ec2Instance, err := ec2.NewInstance(ctx, "ec2Instance", &ec2.InstanceArgs{
 			Ami:                 pulumi.String(ami.Id),
 			InstanceType:        pulumi.String("t2.micro"),
-			SubnetId:            pulumi.String("subnet-087f34630104dcba1"),
+			SubnetId:            pulumi.String(SubnetId),
 			VpcSecurityGroupIds: pulumi.StringArray{sg.ID()},
 			Tags: pulumi.StringMap{
 				"Name": pulumi.String("Pulumi-example"),
